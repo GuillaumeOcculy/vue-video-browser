375fba8438c880e650f830bc838b0b3e5b03ef4a
Guillaume Occuly <occuly.guillaume@gmail.com>
2020-08-27 20:45:58 +0200

    Call youtube API

---
+++ src/components/VideoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class='list-group col-md-4'>
+    <VideoListItem v-for='video in videos' :video='video' :key='video.etag' @videoSelect='onVideoSelect'/>
+  </ul>  
+  
+</template>
+
+<script>
+import VideoListItem from './VideoListItem'
+
+export default {
+  name: 'VideoList',
+  props: ['videos'],
+  components: {VideoListItem},
+  methods: {
+    onVideoSelect(video) {
+      this.$emit('videoSelect', video);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
